dc49b8b91ba5adfbd81c84012e280795b58d8f1c
bashiulAlam <sabab.ndc@gmail.com>
2022-08-15 13:07:06 +0600

    project initiated with completed automation task

--- src/test/java/com/assignment/endtoendtest/utils/Constants.java
+++ src/test/java/com/assignment/endtoendtest/utils/Constants.java
@@ -1,18 +1,6 @@
 package com.assignment.endtoendtest.utils;
 
 public class Constants {
-    private static Constants constants_instance = null;
-
-    private Constants() {
-    }
-
-    public static Constants getInstance() {
-        if (constants_instance == null) {
-            constants_instance = new Constants();
-        }
-        return constants_instance;
-    }
-
     public static final String PROPERTY_FILE_NAME = "config";
     static final String SERVER_IP = "BASE_URL";
     static final String TOKEN = "API_KEY";
